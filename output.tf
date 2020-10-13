ef71241d8adc29d47350f80488322c6b2dd55931
jykingston <jy.kingston@gmail.com>
2020-10-13 14:09:55 +1100

    Add documentation

--- output.tf
+++ output.tf
@@ -1,15 +1,19 @@
 output "hosted_zone" {
   value = aws_route53_zone.hosted_zone
+  description = "HostedZone Object"
 }
 
-output "hosted_zone_id" {
+output "zone_id" {
   value = aws_route53_zone.hosted_zone.zone_id
+  description = "The Hosted Zone ID. This can be referenced by zone records."
 }
 
-output "hosted_zone_name" {
+output "name" {
   value = aws_route53_zone.hosted_zone.name
+  description = "The name of the Hosted Zone"
 }
 
-output "hosted_zone_name_server" {
+output "name_servers" {
   value = aws_route53_zone.hosted_zone.name_servers
+  description = "A list of name servers in associated (or default) delegation set. Find more about delegation sets in AWS docs."
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,9 @@
 variable "domain_name" {
   type = string
+  description = "This is the name of the hosted zone."
 }
+
 variable "comment" {
   type = string
+  description = "A comment for the hosted zone."
 }
